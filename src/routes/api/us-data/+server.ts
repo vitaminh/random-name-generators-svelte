c482e159b666303169fd6b4d9b5cedf4dbc7abdd
Henry Do <henry.do@gmail.com>
2023-09-27 00:20:47 -0700

    Add debugging statements to endpoint

--- src/routes/api/us-data/+server.ts
+++ src/routes/api/us-data/+server.ts
@@ -2,8 +2,13 @@ import { json } from '@sveltejs/kit';
 import { SITE_URL } from '../../../utlities';
 
 export async function GET() {
-	const response = await fetch(`${SITE_URL}/data/names/firstnames/femaleFirstNamesByYear/1880.json`);
-	return response;
+	try {
+		const response = await fetch(`${SITE_URL}/data/names/firstnames/femaleFirstNamesByYear/1880.json`);
+		return response;
+	} catch (error) {
+		console.error(error);
+		console.log(SITE_URL);
+	}
 }
 
 export const prerender = false;
