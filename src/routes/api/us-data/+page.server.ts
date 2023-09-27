f684c28a21caf49467557523cd94e0069d939d79
Henry Do <henry.do@gmail.com>
2023-09-27 16:12:04 -0700

    Switch to Vercel Adapter

--- src/routes/api/us-data/+page.server.ts
+++
@@ -1,4 +0,0 @@
-export const load = async (event) => {
-	const context = event.platform.context;
-	console.log(context); // shows up in your functions log in the Netlify app
-};
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			edge: false,
 			split: true
 		})
 	}
