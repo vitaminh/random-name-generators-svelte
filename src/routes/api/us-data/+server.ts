015a071dceb1588c8ac8eca896c5759f067d9389
Henry Do <henry.do@gmail.com>
2023-09-27 16:17:58 -0700

    Debugging API routes

--- src/routes/api/us-data/+server.ts
+++ src/routes/api/us-data/+server.ts
@@ -2,11 +2,12 @@ import { json } from '@sveltejs/kit';
 // import { SITE_URL } from '../../../utlities';
 import { PUBLIC_SITE_BASE_URL } from '$env/static/public';
 
-export async function GET() {
+// /** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
 	try {
 		const response = await fetch(`${PUBLIC_SITE_BASE_URL}/data/names/firstnames/femaleFirstNamesByYear/1880.json`);
 		return response;
-	} catch (error) {
+	} catch (error: any) {
 		console.error(error);
 		console.log(PUBLIC_SITE_BASE_URL);
 		return error;
