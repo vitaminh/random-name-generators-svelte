ddab72ea469abf4b1cf89308c9f76910de8c14bd
Henry Do <henry.do@gmail.com>
2023-09-26 23:50:39 -0700

    Disable prerendering for API route

--- src/routes/api/us-data/+server.ts
+++ src/routes/api/us-data/+server.ts
@@ -2,9 +2,8 @@ import { json } from '@sveltejs/kit';
 import { SITE_URL } from '../../../utlities';
 
 export async function GET() {
-
 	const response = await fetch(`${SITE_URL}/data/names/firstnames/femaleFirstNamesByYear/1880.json`);
 	return response;
 }
 
-export const prerender = true;
+export const prerender = false;
