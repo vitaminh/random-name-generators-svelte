2bbc91328307f1b72437ea2687640ba692aedf05
Henry Do <henry.do@gmail.com>
2023-09-28 17:22:32 -0700

    Return proper Response object

--- src/routes/api/us-data/+server.ts
+++ src/routes/api/us-data/+server.ts
@@ -2,12 +2,16 @@ import { json } from '@sveltejs/kit';
 // import { SITE_URL } from '../../../utlities';
 import { PUBLIC_SITE_BASE_URL } from '$env/static/public';
 
-// /** @type {import('./$types').RequestHandler} */
-export async function GET({ url }) {
+/** @type {import('./$types').RequestHandler} */
+export async function GET() {
 	try {
 		// const response = await fetch(`${PUBLIC_SITE_BASE_URL}/data/names/firstnames/femaleFirstNamesByYear/1880.json`);
-		const response = await fetch(`${PUBLIC_SITE_BASE_URL}/data/testArray.json`);
-		return response;
+		const response: Response = await fetch(`${PUBLIC_SITE_BASE_URL}/data/testArray.json`);
+		const subArray: string[] = await response.json();
+		console.log('subArray:');
+		console.log(subArray);
+		// return response;
+		return json(subArray);
 	} catch (error: any) {
 		console.error(error);
 		console.log(PUBLIC_SITE_BASE_URL);
