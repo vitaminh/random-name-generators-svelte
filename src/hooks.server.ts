6605ac1a6c6b80305c28249163ee3be31ad4fd81
Henry Do <henry.do@gmail.com>
2023-09-29 16:19:18 -0700

    Alternative CORS implementation

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,47 +4,49 @@ import { PUBLIC_SITE_BASE_URL } from '$env/static/public';
 
 export const handle: Handle = async ({ resolve, event }) => {
 	// const validDomains: RegExp = /^(.*)?\.?example\.com$/;
-	const validDomains: RegExp = new RegExp(PUBLIC_SITE_BASE_URL);
-	let cors: string | null = null;
-	let originDomain = null;
+	// const validDomains: RegExp = new RegExp(PUBLIC_SITE_BASE_URL);
+	// let cors: string | null = null;
+	// let originDomain = null;
 
-	if (event.request.headers.get('origin')) {
-		try {
-			console.log('validating original domain');
-			console.log(event.request.headers.get('origin'));
-			originDomain = new URL(event.request.headers.get('origin') || '').hostname;
-			// const originHeader = event.request.headers.get('origin');
+	// if (event.request.headers.get('origin')) {
+	// 	try {
+	// 		console.log('validating original domain');
+	// 		console.log(event.request.headers.get('origin'));
+	// 		originDomain = new URL(event.request.headers.get('origin') || '').hostname;
+	// 		// const originHeader = event.request.headers.get('origin');
 
-			// originDomain = new URL(event.request.headers.get('origin') ?? '').hostname;
-			console.log('originDomain: ' + originDomain);
-			if (validDomains.test(originDomain)) {
-				cors = originDomain;
-			}
-		} catch (e) {
-			console.log('Invalid origin', e);
-		}
-	}
+	// 		// originDomain = new URL(event.request.headers.get('origin') ?? '').hostname;
+	// 		console.log('originDomain: ' + originDomain);
+	// 		if (validDomains.test(originDomain)) {
+	// 			cors = originDomain;
+	// 		}
+	// 	} catch (e) {
+	// 		console.log('Invalid origin', e);
+	// 	}
+	// }
 
 
-	// Apply CORS header for API routes
-	if (event.url.pathname.startsWith('/api')) {
-		// Required for CORS to work
-		// if(event.request.method === 'OPTIONS') {
-		if (event.request.method === 'OPTIONS' && cors) {
-			return new Response(null, {
-				headers: {
-					'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-					'Access-Control-Allow-Origin': '*',
-					'Access-Control-Allow-Headers': '*',
-				}
-			});
-		}
-	}
+	// // Apply CORS header for API routes
+	// if (event.url.pathname.startsWith('/api')) {
+	// 	// Required for CORS to work
+	// 	// if(event.request.method === 'OPTIONS') {
+	// 	if (event.request.method === 'OPTIONS' && cors) {
+	// 		return new Response(null, {
+	// 			headers: {
+	// 				'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+	// 				'Access-Control-Allow-Origin': '*',
+	// 				'Access-Control-Allow-Headers': '*',
+	// 			}
+	// 		});
+	// 	}
+	// }
 
 	const response = await resolve(event);
 
-	if (event.url.pathname.startsWith('/api')) {
-		response.headers.append('Access-Control-Allow-Origin', `cors`);
-	}
+	response.headers.append('Access-Control-Allow-Origin', PUBLIC_SITE_BASE_URL);
+
+	// if (event.url.pathname.startsWith('/api')) {
+	// 	response.headers.append('Access-Control-Allow-Origin', `cors`);
+	// }
 	return response;
 };
