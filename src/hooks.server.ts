54ff39353c10bb5c1a419469d6d6826015f8cc63
Henry Do <henry.do@gmail.com>
2023-10-02 01:52:24 -0700

    add error handling

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,11 @@ export const handle: Handle = async ({ resolve, event }) => {
 	console.log('hostname: ' + hostname);
 	console.log( `host matches: ${ hostname == PUBLIC_SITE_HOSTNAME}`);
 	console.log('isSubRequest: ' + event.isSubRequest);
-	console.log('client address: ' + event.getClientAddress());
+	try {
+		console.log('client address: ' + event.getClientAddress());
+	} catch (error) {
+		console.error(error);
+	}
 	console.log(event.request);
 	console.log(event.request.headers);
 	event.request.headers.forEach(header => console.log(header));
