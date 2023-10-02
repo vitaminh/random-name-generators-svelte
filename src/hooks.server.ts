e0c4d0ff3b0ec5c59e806c0e3f14ce2d0008d5ef
Henry Do <henry.do@gmail.com>
2023-10-02 01:50:22 -0700

    Add more logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,5 +7,9 @@ export const handle: Handle = async ({ resolve, event }) => {
 	console.log('hostname: ' + hostname);
 	console.log( `host matches: ${ hostname == PUBLIC_SITE_HOSTNAME}`);
 	console.log('isSubRequest: ' + event.isSubRequest);
+	console.log('client address: ' + event.getClientAddress());
+	console.log(event.request);
+	console.log(event.request.headers);
+	event.request.headers.forEach(header => console.log(header));
 	return await resolve(event);
 };
