24985e18975c69933cc2045ec53e1f94ce32b023
Henry Do <henry.do@gmail.com>
2023-10-02 02:05:23 -0700

    Add referer logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,17 +3,10 @@ import type { Handle } from '@sveltejs/kit';
 import { PUBLIC_SITE_HOSTNAME } from '$env/static/public';
 
 export const handle: Handle = async ({ resolve, event }) => {
-	const hostname: string | null = event.request.headers.get('host');
-	console.log('hostname: ' + hostname);
-	console.log( `host matches: ${ hostname == PUBLIC_SITE_HOSTNAME}`);
-	console.log('isSubRequest: ' + event.isSubRequest);
-	try {
-		console.log('client address: ' + event.getClientAddress());
-	} catch (error) {
-		console.error(error);
-	}
 	console.log(event.request);
 	console.log(event.request.headers);
 	event.request.headers.forEach(header => console.log(header));
+	const referer = event.request.headers.get('referer');
+	console.log('referer: ' + referer);
 	return await resolve(event);
 };
